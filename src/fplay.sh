f5c08ff9ca2f574f5183cf62c56a56a38c25f8e3
simonhughcom <simonhughdev@gmail.com>
2019-12-10 19:26:35 +0000

    Choose max depth in first argument.

--- src/fplay.sh
+++ src/fplay.sh
@@ -4,12 +4,18 @@
 # Simon Hugh Moore
 #
 # Lists all video media (mkv, mp4, mp3 etc) and plays them in mpv.
+# 
+# You can choose max depth of directory in first argument.
+# Default max depth is 5.
 
 extensions='mkv|mp4|mp3|mov'
 
+# get max depth to traverse for files.
+[ $1 ] && depth="$1" || depth="5"
+
 get_files(){
     if command -v fd > /dev/null; then
-        fd "^.*\.($extensions)" -d 5 -t f 
+        fd "^.*\.($extensions)" -d "$depth" -t f 
     else
         echo "Currently does not work with find!"
     fi
