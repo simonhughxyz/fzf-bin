f72e2c8e9a2304790ef1fcc80b42401968ffe2f1
Simon H Moore <simonhughdev@gmail.com>
2019-12-30 18:06:17 +0000

    Can choode depth with -d flag and extensions with -e flag.

--- src/fplay.sh
+++ src/fplay.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 # FPLAY
 # Simon Hugh Moore
@@ -9,9 +9,10 @@
 # Default max depth is 5.
 
 extensions='mkv|mp4|mp3|mp2|mov|mpg|mpeg|mpv|mpe|ogg|oga|ogv|m4p|m4v|avi|wmv|mov|qt|flv|gif|webm|aac|wma|flac|alac|wav'
+depth="5"
 
 # get max depth to traverse for files.
-[ $1 ] && depth="$1" || depth="5"
+# [ $1 ] && depth="$1" || depth="5"
 
 get_files(){
     if command -v fd > /dev/null; then
@@ -20,6 +21,15 @@ get_files(){
         echo "Currently does not work with find!"
     fi
 }
+
+# get user options
+while getopts "e:d:" flag; do
+    case "${flag}" in
+        e) extensions=${OPTARG};;
+        d) depth="${OPTARG}";;
+    esac
+done
+shift $((OPTIND-1))
 
 choice="$(get_files | fzf --prompt="$(pwd)/")" || exit
 
