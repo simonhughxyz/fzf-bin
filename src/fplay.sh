a5a7ca0e3a77efd778a7915c2ba5716ad117dbc0
Simon H Moore <simonhughdev@gmail.com>
2019-12-30 18:21:09 +0000

    added help option.

--- src/fplay.sh
+++ src/fplay.sh
@@ -6,8 +6,15 @@
 # Lists all media (mkv, mp4, mp3 etc) and plays them in mpv.
 # 
 # Options:
-# -e    : Choose extensions to find (have to be seperated by `|`).
+# -e    : Choose extensions to find (have to be separated by `|`).
 # -d    : Choose directory depth to traverse, default is 5.
+
+usage="$(basename "$0") [-h] [-e s] [-d n] -- find media files and play them in mpv.
+
+where:
+    -h  show this help text.
+    -e  set the extensions to find (have to be separated by \`|\`)
+    -d  set directory depth to traverse (default: 5)"
 
 extensions='mkv|mp4|mp3|mp2|mov|mpg|mpeg|mpv|mpe|ogg|oga|ogv|m4p|m4v|avi|wmv|mov|qt|flv|gif|webm|aac|wma|flac|alac|wav'
 depth="5"
@@ -24,8 +31,9 @@ get_files(){
 }
 
 # get user options
-while getopts "e:d:" flag; do
+while getopts "he:d:" flag; do
     case "${flag}" in
+        h) echo "$usage"; exit;;
         e) extensions=${OPTARG};;
         d) depth="${OPTARG}";;
     esac
